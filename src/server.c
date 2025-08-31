fd66097064612ecbb1f0a169e7ec4103477828df
LucasHerdegen <lherdegen@frba.utn.edu.ar>
2025-08-31 12:28:11 -0300

    feat: parse request

--- src/server.c
+++ src/server.c
@@ -56,6 +56,26 @@ void* handler_cliente(void* client_socket)
     LOG_INFO("Peticion recibida: ");
     fprintf(stderr, "%s\n", buffer);
 
+    HttpRequest req = parse_request(buffer);
+
+    LOG_INFO("Metodo: ");
+    fprintf(stderr, "%s\n", req.method);
+    LOG_INFO("Path: ");
+    fprintf(stderr, "%s\n", req.path);
+    LOG_INFO("Version: ");
+    fprintf(stderr, "%s\n", req.version);
+
+    sleep(3);
+
+    parse_headers(buffer, &req);
+
+    for (int i = 0; i < req.header_count; i++)
+    {
+        fprintf(stderr, "Header[%d]: %s\n", i, req.headers[i]);
+    }
+
+    sleep(3);
+
     char *resp = 
     "HTTP/1.1 200 OK\r\n"
     "Content-Type: text/plain\r\n"
@@ -70,4 +90,41 @@ void* handler_cliente(void* client_socket)
     
     close(socket);
     return NULL;
+}
+
+HttpRequest parse_request(const char* request)
+{
+    HttpRequest req;
+    memset(&req, 0, sizeof(HttpRequest));
+
+    char first_line[256];
+    sscanf(request, "%[^\r\n]", first_line);
+    
+    sscanf(first_line, "%s %s %s", req.method, req.path, req.version);
+
+    return req;
+}
+
+void parse_headers(const char* request, HttpRequest* req) {
+    req->headers = NULL;
+    req->header_count = 0;
+
+    const char* headers_start = strstr(request, "\r\n") + 2;
+    const char* current = headers_start;
+
+    while (current && *current != '\r' && *current != '\n')
+    {
+        const char* line_end = strstr(current, "\r\n");
+        if (!line_end) break;
+
+        size_t line_len = line_end - current;
+        char* header = malloc(line_len + 1);
+        strncpy(header, current, line_len);
+        header[line_len] = '\0';
+
+        req->headers = realloc(req->headers, sizeof(char*) * (req->header_count + 1));
+        req->headers[req->header_count++] = header;
+
+        current = line_end + 2;
+    }
 }
--- src/server.h
+++ src/server.h
@@ -1,3 +1,16 @@
 #include "sockets.h"
 
+typedef struct
+{
+    char method[8]; // "GET", "POST"
+    char path[256]; // "/" "/index.html"
+    char version[16]; // "HTTP/1.1"
+    char** headers;
+    int header_count;
+} HttpRequest;
+
 void* handler_cliente(void* client_socket);
+
+HttpRequest parse_request(const char* request);
+
+void parse_headers(const char* request, HttpRequest* req);
